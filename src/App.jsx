09709bec1dc95c9dca00587d0cd868e55aca8051
Dilip Kumar R <dilipkumar2000.r@gmail.com>
2025-11-06 18:38:25 +0530

    init for ranvita.

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div>
+        <h4> hang tight </h4>
+        <i>
+          we will update this shortly..
+        </i>
+      </div>
+    </>
+  )
+}
+
+export default App
